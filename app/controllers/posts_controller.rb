e505f9c7f8de28af4c4e7ba7d4551a94e6621763
snowdevin.sd@gmail.com <snowdevin.sd@gmail.com>
2019-09-01 01:17:33 +0200

    added identation within controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -67,13 +67,13 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:image,:description)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def post_params
+    params.require(:post).permit(:image,:description)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name,:last_name, :email_or_phone, :password,
-      :password_confirmation, :date, :gender)
+    params.require(:user).permit(:first_name,:last_name, :email_or_phone, :password, :password_confirmation, :date, :gender)
   end
 end
